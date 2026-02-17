35e1904ac7cb6b55c39c0d5f16e657b3e1841ef1
Shane Duffy <shane@shaneduffy.io>
2026-02-16 17:48:06 -0800

    Update landing background

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { BlogListItemComponent } from './components/blog-list-item/blog-list-ite
 import { AboutComponent } from './components/about/about.component'
 import { ProjectsComponent } from './components/projects/projects.component'
 import { HeaderComponent } from './components/header/header.component';
+import { InvadersBackgroundComponent } from './components/header/invaders-background/invaders-background.component';
 import { NotesPostComponent } from './components/notes-post/notes-post.component';
 import { NotesComponent } from './components/notes/notes.component';
 
@@ -38,6 +39,7 @@ import { SafePipe } from './pipes/safe.pipe';
     AboutComponent,
     ProjectsComponent,
     HeaderComponent,
+    InvadersBackgroundComponent,
     SafePipe
   ],
   imports: [
